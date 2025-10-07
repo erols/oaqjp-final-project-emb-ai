9dd6e74a9e15734f4a8674171eb83ba343cec205
erols <erol.suleyman@gmail.com>
2025-10-07 06:38:02 -0400

    task 3 done

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,7 +8,13 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=input_json, headers=headers)
 
     if response.status_code == 200:
-        return response.text
+        formatted_response = json.loads(response.text)
+        result = formatted_response['emotionPredictions'][0]['emotion']
+        emotions = list(result.keys())
+        scores = list(result.values())
+        dominant_emotion = emotions[scores.index(max(scores))]
+        result['dominant_emotion'] = dominant_emotion
+        return result
 
     if response.status_code == 500:
         print("server error")
